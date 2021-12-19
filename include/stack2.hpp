5e8b0c000b6a935c2c9135723243282995e6edda
SovesT1337 <temirkhan2002@gmail.com>
2021-12-19 17:37:55 +0300

    VI Commit

--- include/stack2.hpp
+++ include/stack2.hpp
@@ -45,8 +45,7 @@ class Stack2 {
     }
     delete[] ptr;
     ptr = new T[++length];
-    for (unsigned int i = 0; i < length; ++i) 
-        ptr[i] = forward<T>(data[i]);
+    for (unsigned int i = 0; i < length; ++i) ptr[i] = forward<T>(data[i]);
     delete data;
     ptr[length - 1] = forward<T>(value);
   }
